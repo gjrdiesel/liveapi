374ad6eae658893e90f4e0617e7bfbbb2886986b
Justin Reasoner <knoxxbox@gmail.com>
2023-01-28 08:56:48 -0500

    long lived browser

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const playwright = require('playwright');
 const { json } = require('micro')
 
+const browser = await playwright.chromium.launch();
+const context = await browser.newContext();
+const page = await context.newPage();
+
 module.exports = async (req, res) => {
-    const browser = await playwright.chromium.launch();
-    const context = await browser.newContext();
-    const page = await context.newPage();
-
     const {query} = await json(req);
+    
     await page.goto(`https://www.google.com/search?q=${query}`);
-
-    const response = await page.evaluate(query => {
+    
+    return await page.evaluate(query => {
         return {
             query,
             text: document.querySelector('c-wiz div[aria-live="polite"]')?.innerText.replace('Live: ', ''), // "Little less than busy"
@@ -19,8 +20,4 @@ module.exports = async (req, res) => {
             }
         }
     }, query);
-
-    await browser.close();
-
-    return response;
 };
